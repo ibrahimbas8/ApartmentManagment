a3842700967ae39fe5c2d3b9df3e3ed0b3883fd0
İbrahim Baş <bas.ibrahim98@gmail.com>
2022-12-16 15:54:47 +0300

    Create Context Class

---
+++ ApartmentManagment/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<ExpenseType> ExpenseTypes { get; set; }
+        public DbSet<Flat> Flats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}

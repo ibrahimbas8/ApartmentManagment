9b49faef9c40c1bc522d458971742c69a78991b3
İbrahim Baş <bas.ibrahim98@gmail.com>
2022-12-16 19:11:07 +0300

    Create SignInViewModel

---
+++ ApartmentManagment/ApartmentManagment/Models/RoleUpdateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ApartmentManagment.Models
+{
+    public class RoleUpdateViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ApartmentManagment/ApartmentManagment/Models/UserSignInViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApartmentManagment.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adını Giriniz...")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Lütfen Kullanıcı Şifreyi Giriniz...")]
+        public string Password { get; set; }
+    }
+}
